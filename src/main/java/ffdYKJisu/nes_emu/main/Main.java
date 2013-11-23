7c8341f86cf76197d1ce74fda6dec9506a4a50f1
Frank <phranck+github@gmail.com>
2013-11-23 08:37:06 -0800

    more migration to inputstream

--- src/main/java/ffdYKJisu/nes_emu/main/Main.java
+++ src/main/java/ffdYKJisu/nes_emu/main/Main.java
@@ -4,69 +4,58 @@
  */
 package ffdYKJisu.nes_emu.main;
 
-
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import ffdYKJisu.nes_emu.system.NES;
 
 /**
- *
+ * 
  * @author fe01106
  */
 public class Main {
 
     /**
-     * @param args the command line arguments
+     * @param args
+     *            the command line arguments
      */
     public static void main(String[] args) {
         String romName = "Pac-Man (U) [!].nes";
-        
-        BufferedReader reader = new BufferedReader(new InputStreamReader(
-                Main.class.getClass().getClassLoader().getResourceAsStream(
-                    romName)));
-        
-		NES nes = new NES();
-		nes.loadRom(new File(romName));
-		nes.initialize();
-		nes.emulateFor(20);
-		/*
-		d.loadRom(romName);
-		d.startConsole();
-		 */
-		/*
-        
-        File cart = new File(romName);
+
+        InputStream romIs = Main.class.getClass().getClassLoader()
+                .getResourceAsStream(romName);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(romIs));
+
         NES nes = new NES();
-        nes.loadRom(cart);
+        nes.loadRom(romIs);
         nes.initialize();
-        nes.emulateFor(100);
-		 */
-		
+        nes.emulateFor(20);
         /*
-        CPU cpu = new CPU();
-        cpu.loadRom(cart);
-        cpu.init();
-           */
+         * d.loadRom(romName); d.startConsole();
+         */
+        /*
+         * 
+         * File cart = new File(romName); NES nes = new NES();
+         * nes.loadRom(cart); nes.initialize(); nes.emulateFor(100);
+         */
+
+        /*
+         * CPU cpu = new CPU(); cpu.loadRom(cart); cpu.init();
+         */
         // cpu.emulateFor(100);
-    // uByte test harness
+        // uByte test harness
         /*
-        uByte temp;
-        for ( int i = 0; i < 0x100; i++) {
-            temp = new uByte(i);
-            // System.out.print(temp + " ");
-            System.out.print(temp + "->" + temp.negativeValue() + " neg? "
-                    + temp.isNegative() + " as signed byte " + temp.toSigned()
-                    + "\n");
-        }
-        */
-        /*System.out.println("uByte test harness");
-        uByte a = new uByte(2323);
-        for(int i =0; i > -1000; i--) {
-        a.set(i);
-        System.out.print(a.toString() + " ");
-        }
-        */
+         * uByte temp; for ( int i = 0; i < 0x100; i++) { temp = new uByte(i);
+         * // System.out.print(temp + " "); System.out.print(temp + "->" +
+         * temp.negativeValue() + " neg? " + temp.isNegative() +
+         * " as signed byte " + temp.toSigned() + "\n"); }
+         */
+        /*
+         * System.out.println("uByte test harness"); uByte a = new uByte(2323);
+         * for(int i =0; i > -1000; i--) { a.set(i);
+         * System.out.print(a.toString() + " "); }
+         */
     }
 }
--- src/main/java/ffdYKJisu/nes_emu/system/Cartridge.java
+++ src/main/java/ffdYKJisu/nes_emu/system/Cartridge.java
@@ -13,13 +13,18 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.apache.commons.lang.ArrayUtils;
+
+import com.google.common.collect.Lists;
 
 import ffdYKJisu.nes_emu.exceptions.bankNotFoundException;
 
 /**
- *
+ * 
  * @author fe01106
  */
 public class Cartridge {
@@ -33,12 +38,12 @@ public class Cartridge {
     public Cartridge(InputStream is) {
         this.loadRom(is);
     }
-    
+
     public Cartridge(File file) {
         this.loadRomFromFile(file);
         this.setValuesFromHeader();
     }
-    
+
     /**
      * Defines the various kinds of banks possible in a cartridge
      * <p>
@@ -50,9 +55,9 @@ public class Cartridge {
      */
     enum Bank {
 
-        PRG16(0x4000), //16KB
+        PRG16(0x4000), // 16KB
         CHR8(0x2000), // 8KB
-        RAM8(0x2000);	// 8KB
+        RAM8(0x2000); // 8KB
         public final int length;
 
         Bank(double size) {
@@ -63,32 +68,34 @@ public class Cartridge {
     enum Mirroring {
         VERTICAL, HORIZONTAL
     };
-    
+
     /** Stores the mirroring (horizontal/vertical) of the cartridge */
     Mirroring mirroring;
-    
+
     Byte getByte(int index) {
         try {
             return (Byte) romData[index];
         } catch (ArrayIndexOutOfBoundsException ex) {
-            Logger.getLogger(Cartridge.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Cartridge.class.getName()).log(Level.SEVERE, null,
+                    ex);
         }
         return 0;
     }
 
-    void loadRom(InputStream is) {
-        
+    public void loadRom(InputStream is) {
+        romData = getBytesFromInput(is);
     }
-    
-    void loadRomFromFile(File file) {
+
+    public void loadRomFromFile(File file) {
         try {
             romData = getBytesFromInput(new FileInputStream(file));
         } catch (IOException ex) {
-            Logger.getLogger(Cartridge.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Cartridge.class.getName()).log(Level.SEVERE, null,
+                    ex);
         }
     }
 
-    byte[] getBytesFromInput(InputStream is) throws IOException {
+    private byte[] getBytesFromInput(InputStream is) throws IOException {
         // You cannot create an array using a long type.
         // It needs to be an int type.
         // Before converting to an int type, check
@@ -96,38 +103,19 @@ public class Cartridge {
 
         // Create the byte array to hold the data
         List<Byte> bytes = Lists.newArrayList();
-        byte[] bytes = new byte[(int) length];
-        FileInputStream fis = null;
-        BufferedInputStream bis = null;
-        DataInputStream dis = null;
 
-        try {
-            fis = new FileInputStream(file);
-
-            // Here BufferedInputStream is added for fast reading.
-            bis = new BufferedInputStream(fis);
-            dis = new DataInputStream(bis);
-            int i = 0;
-            // dis.available() returns 0 if the file does not have more lines.
-            while (dis.available() != 0) {
-
-                // this statement reads the line from the file and print it to
-                // the console.
-
-                dis.readFully(bytes);
+        boolean isDoneReading = false;
+        // dis.available() returns 0 if the file does not have more lines.
+        while (!isDoneReading) {
+            int byteValue = is.read();
+            if (byteValue != -1) {
+                bytes.add((byte) byteValue);
+            } else {
+                isDoneReading = true;
             }
-
-            // dispose all the resources after using them.
-            fis.close();
-            bis.close();
-            dis.close();
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return bytes;
+        is.close();
+        return ArrayUtils.toPrimitive(bytes.toArray(new Byte[] {}));
     }
 
     String printcartridgeData() {
@@ -139,14 +127,16 @@ public class Cartridge {
             try {
                 temp = this.get16PRGBank(i);
             } catch (bankNotFoundException ex) {
-                Logger.getLogger(Cartridge.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(Cartridge.class.getName()).log(Level.SEVERE,
+                        null, ex);
             }
             for (int j = 0; j < temp.length; j++) {
-                //System.out.printf("%X", temp[j]);
+                // System.out.printf("%X", temp[j]);
                 output.append(String.format("%X", temp[j]));
             }
         }
-        //System.out.print("There are " + this.num8CHRBanks + " CHR8 Bank(s)\n");
+        // System.out.print("There are " + this.num8CHRBanks +
+        // " CHR8 Bank(s)\n");
         output.append("\nThere are " + this.num8CHRBanks + " CHR8 Bank(s)\n");
         for (int i = 0; i < this.num8CHRBanks; i++) {
             output.append("CHR8 Bank " + i + "\n");
@@ -154,10 +144,11 @@ public class Cartridge {
             try {
                 temp = this.get8CHRBank(i);
             } catch (bankNotFoundException ex) {
-                Logger.getLogger(Cartridge.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(Cartridge.class.getName()).log(Level.SEVERE,
+                        null, ex);
             }
             for (int j = 0; j < temp.length; j++) {
-                //System.out.printf("%X", temp[j]);
+                // System.out.printf("%X", temp[j]);
                 output.append(String.format("%X", temp[j]));
             }
             output.append("\n");
@@ -165,14 +156,15 @@ public class Cartridge {
         return output.toString();
     }
 
-    @Override public String toString() {
+    @Override
+    public String toString() {
         StringBuffer sb = new StringBuffer();
         sb.append("");
         sb.append(printHeaders());
         sb.append(printcartridgeData());
         return sb.toString();
     }
-    
+
     /**
      * Read cartridge and read iNes header and store to cartridge
      */
@@ -193,6 +185,7 @@ public class Cartridge {
 
     /**
      * Returns the values read from the iNES header as a String
+     * 
      * @return Values from the iNES header presented in an easy-to-read String
      */
     String printHeaders() {
@@ -201,43 +194,50 @@ public class Cartridge {
         sb.append(num8CHRBanks + " 8KB CHR Banks" + "\n");
         sb.append(num8RAMBanks + " 8KB RAM Banks" + "\n");
         sb.append(mirroring + " Mirroring" + "\n");
-        //System.out.print(sb.toString());
+        // System.out.print(sb.toString());
         return sb.toString();
     }
 
     /**
      * Reads the specified PRG-ROM bank from the cartridge and returns a Byte[]
      * array of the bytes contained in the bank. Always 16KB in size.
-     * @param bankNum Number of the bank you want from the cartridge starting
-     * from zero
+     * 
+     * @param bankNum
+     *            Number of the bank you want from the cartridge starting from
+     *            zero
      * @return Byte array of the specific bank requested
      * @throws nes.bankNotFoundException
      */
     Byte[] get16PRGBank(int bankNum) throws bankNotFoundException {
         if (bankNum > this.num16PRGBanks) {
-            throw new bankNotFoundException("Bank " + bankNum + " doesn't exist");
+            throw new bankNotFoundException("Bank " + bankNum
+                    + " doesn't exist");
         }
         int bankLength = Bank.PRG16.length;
         Byte[] bank = new Byte[bankLength];
         for (int i = 0; i < bankLength; i++) {
-            //System.out.println("Bank length " + bankLength + " Bank offset " +
-            //	this.getBankOffset(Bank.PRG16, bankNum) + " index " + i);
+            // System.out.println("Bank length " + bankLength + " Bank offset "
+            // +
+            // this.getBankOffset(Bank.PRG16, bankNum) + " index " + i);
             bank[i] = romData[this.getBankOffset(Bank.PRG16, bankNum) + i];
         }
         return bank;
     }
 
-        /**
+    /**
      * Reads the specified CHR-ROM bank from the cartridge and returns a Byte[]
      * array of the bytes contained in the bank. Always 8KB in size.
-     * @param bankNum Number of the bank you want from the cartridge starting
-     * from zero
+     * 
+     * @param bankNum
+     *            Number of the bank you want from the cartridge starting from
+     *            zero
      * @return Byte array of the specific bank requested
      * @throws nes.bankNotFoundException
      */
     Byte[] get8CHRBank(int bankNum) throws bankNotFoundException {
         if (bankNum > this.num8CHRBanks) {
-            throw new bankNotFoundException("Bank " + bankNum + " doesn't exist");
+            throw new bankNotFoundException("Bank " + bankNum
+                    + " doesn't exist");
         }
         int bankLength = Bank.CHR8.length;
         Byte[] bank = new Byte[bankLength];
@@ -251,9 +251,8 @@ public class Cartridge {
         if (bankType.equals(Bank.PRG16)) {
             return this.iNESOffset + Bank.PRG16.length * bankNum;
         } else if (bankType.equals(Bank.CHR8)) {
-            int offset = this.iNESOffset +
-                    Bank.PRG16.length * this.num16PRGBanks +
-                    Bank.CHR8.length * bankNum;
+            int offset = this.iNESOffset + Bank.PRG16.length
+                    * this.num16PRGBanks + Bank.CHR8.length * bankNum;
             return offset;
         } else {
             return this.iNESOffset;
