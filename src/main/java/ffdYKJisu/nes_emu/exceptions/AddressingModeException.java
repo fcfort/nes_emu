c2bb3021965e25924a64b7f8908fc3c11d3f3b02
ffdYKJisu <phranck+github@gmail.com>
2013-11-28 12:17:59 -0500

    commit of new exception

---
+++ src/main/java/ffdYKJisu/nes_emu/exceptions/AddressingModeException.java
@@ -0,0 +1,12 @@
+package ffdYKJisu.nes_emu.exceptions;
+
+public class AddressingModeException extends RuntimeException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1923103205866993061L;
+
+	public AddressingModeException(String msg) {
+          super(msg);
+	}
+}
